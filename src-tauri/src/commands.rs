10cf5c0fb8e8c0faadb68dbd1df30af742d869c1
lanyeeee <lanyeeeeee@gmail.com>
2024-09-18 08:36:29 +0800

    refactor: 用单独的文件管理commands

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,14 @@
+use crate::config::Config;
+
+#[tauri::command]
+#[specta::specta]
+pub fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+#[specta::specta]
+#[allow(clippy::needless_pass_by_value)]
+pub fn get_config(config: tauri::State<std::sync::RwLock<Config>>) -> Config {
+    config.read().unwrap().clone()
+}
