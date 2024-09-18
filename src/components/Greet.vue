2259e3771551c75efb1c43e4dd3f8894f288b14c
lanyeeee <lanyeeeeee@gmail.com>
2024-09-18 08:30:16 +0800

    feat: 添加配置文件

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
+import {ref} from "vue";
+import {commands} from "../bindings.ts";
 
 const greetMsg = ref("");
 const name = ref("");
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
+  greetMsg.value = await commands.greet(name.value);
+}
+
+async function test() {
+  const config = await commands.getConfig();
+  console.log(config);
 }
 </script>
 
 <template>
   <form class="row" @submit.prevent="greet">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+    <input id="greet-input" v-model="name" placeholder="Enter a name..."/>
     <button type="submit">Greet</button>
   </form>
+  <button @click="test">测试</button>
 
   <p>{{ greetMsg }}</p>
 </template>
