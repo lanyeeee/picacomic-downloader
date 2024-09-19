6e7e284fb39643d0ad5295e329f8ddb59c2489b0
lanyeeee <1210347077@qq.com>
2024-09-19 19:40:32 +0800

    feat: 用户基本信息展示

--- src/AppContent.vue
+++ src/AppContent.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import {onMounted, ref, watch} from "vue";
-import {commands, Config} from "./bindings.ts";
-import {useMessage} from "naive-ui";
+import {commands, Config, UserProfile} from "./bindings.ts";
+import {useMessage, useNotification} from "naive-ui";
 import LoginDialog from "./components/LoginDialog.vue";
 
 const message = useMessage();
+const notification = useNotification();
 
 const config = ref<Config>();
 const loginDialogShowing = ref<boolean>(false);
+const userProfile = ref<UserProfile>();
 
 watch(config, async () => {
   if (config.value === undefined) {
@@ -16,14 +18,23 @@ watch(config, async () => {
   await commands.saveConfig(config.value);
   message.success("保存配置成功");
 }, {deep: true});
+watch(() => config.value?.token, async () => {
+  const result = await commands.getUserProfile();
+  if (result.status === "error") {
+    notification.error({title: "获取用户信息失败", description: result.error});
+    userProfile.value = undefined;
+    return;
+  }
+  userProfile.value = result.data;
+  message.success("获取用户信息成功");
+});
 
 onMounted(async () => {
   config.value = await commands.getConfig();
 });
 
 async function test() {
-  console.log("test");
-
+  console.log(userProfile.value);
 }
 
 </script>
@@ -38,6 +49,17 @@ async function test() {
       </n-input>
       <n-button type="primary" @click="loginDialogShowing=true">账号登录</n-button>
       <n-button @click="test">测试用</n-button>
+    </div>
+    <div class="flex" v-if="userProfile!==undefined">
+      <n-avatar v-if="userProfile.avatar!==undefined"
+                round
+                :size="50"
+                :src="`${userProfile.avatar.fileServer}/static/${userProfile.avatar.path}`"
+                fallback-src="https://storage-b.picacomic.com/static/b3411e38-32f2-4ec4-a46c-2edee925dbbd.jpg"/>
+      <div class="flex flex-col">
+        <span>{{ userProfile.name }}</span>
+        <span>Lv.{{ userProfile.level }} {{ userProfile.title }}</span>
+      </div>
     </div>
 
     <n-modal v-model:show="loginDialogShowing">
