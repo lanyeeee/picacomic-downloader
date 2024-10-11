c2d0aa6dcb53721a327e9f3376135c1fda82eb05
lanyeeee <lanyeeeeee@gmail.com>
2024-10-12 07:18:29 +0800

    feat: 给漫画卡片添加hover反馈

--- src/components/ComicCard.vue
+++ src/components/ComicCard.vue
@@ -1,32 +1,27 @@
 <script setup lang="ts">
-import {Pagination} from "../bindings.ts";
 import {ComicInfo} from "../types.ts";
 
 defineProps<{
-  comicInfoPagination: Pagination<ComicInfo> | undefined;
+  comicInfo: ComicInfo;
   onClickItem: (comicId: string) => void;
 }>();
 </script>
 
 <template>
-  <div v-if="comicInfoPagination!==undefined" class="flex flex-col gap-row-2 overflow-auto">
-    <n-card class="cursor-pointer"
-            content-style="padding: 0.25rem;"
-            hoverable
-            v-for="comicInfo in comicInfoPagination.docs"
-            :key="comicInfo._id"
-            @click="onClickItem(comicInfo._id)">
-      <div class="flex">
-        <img class="w-24 object-cover pr-4"
-             :src="`${comicInfo.thumb.fileServer}/static/${comicInfo.thumb.path}`"
-             alt=""
-             referrerpolicy="no-referrer"/>
-        <div class="flex flex-col h-full">
-          <span class="font-bold text-xl">{{ comicInfo.title }}</span>
-          <span class="text-red">作者：{{ comicInfo.author }}</span>
-          <span class="text-gray" v-html="`分类：${comicInfo.categories}`"></span>
-        </div>
+  <n-card content-style="padding: 0.25rem;" hoverable>
+    <div class="flex">
+      <img
+          class="w-24 aspect-[3/4] object-contain mr-4 cursor-pointer transform transition-transform duration-200 hover:scale-106"
+          :src="`${comicInfo.thumb.fileServer}/static/${comicInfo.thumb.path}`"
+          alt=""
+          referrerpolicy="no-referrer"
+          @click="onClickItem(comicInfo._id)"/>
+      <div class="flex flex-col w-full">
+        <span class="font-bold text-xl line-clamp-2 cursor-pointer hover:text-blue-5"
+              @click="onClickItem(comicInfo._id)">{{ comicInfo.title }}</span>
+        <span class="text-red">作者：{{ comicInfo.author }}</span>
+        <span class="text-gray" v-html="`分类：${comicInfo.categories}`"></span>
       </div>
-    </n-card>
-  </div>
+    </div>
+  </n-card>
 </template>
