307b2450db6640f8c4e273b8d5cbbf30776fc280
lanyeeee <lanyeeeeee@gmail.com>
2025-04-26 05:34:06 +0800

    deps: 移除用不到的`tauri-plugin-fs`

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -58,7 +58,6 @@ pub async fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(builder.invoke_handler())
         .setup(move |app| {
