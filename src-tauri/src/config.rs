05e06da1c214084ae469d2fa444a269551bc5f50
lanyeeee <lanyeeeeee@gmail.com>
2025-05-01 05:35:50 +0800

    feat: 优化配置文件解析逻辑，支持配置项的向后兼容

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,6 +1,5 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-use anyhow::Context;
 use serde::{Deserialize, Serialize};
 use specta::Type;
 use tauri::{AppHandle, Manager};
@@ -20,27 +19,19 @@ impl Config {
     pub fn new(app: &AppHandle) -> anyhow::Result<Self> {
         let app_data_dir = app.path().app_data_dir()?;
         let config_path = app_data_dir.join("config.json");
-        let default_config = Config {
-            token: String::new(),
-            download_dir: app_data_dir.join("漫画下载"),
-            export_dir: app_data_dir.join("漫画导出"),
-            chapter_download_interval: 0,
-            download_with_author: false,
-            enable_file_logger: true,
-        };
-        // 如果配置文件存在且能够解析，则使用配置文件中的配置，否则使用默认配置
+
         let config = if config_path.exists() {
             let config_string = std::fs::read_to_string(config_path)?;
-            serde_json::from_str(&config_string).unwrap_or(default_config)
+            match serde_json::from_str(&config_string) {
+                // 如果能够直接解析为Config，则直接返回
+                Ok(config) => config,
+                // 否则，将默认配置与文件中已有的配置合并
+                // 以免新版本添加了新的配置项，用户升级到新版本后，所有配置项都被重置
+                Err(_) => Config::merge_config(&config_string, &app_data_dir),
+            }
         } else {
-            default_config
+            Config::default(&app_data_dir)
         };
-        if !config.download_dir.exists() {
-            std::fs::create_dir_all(&config.download_dir).context(format!(
-                "创建下载目录`{}`失败",
-                config.download_dir.display()
-            ))?;
-        }
         config.save(app)?;
         Ok(config)
     }
@@ -51,5 +42,38 @@ impl Config {
         let config_string = serde_json::to_string_pretty(self)?;
         std::fs::write(config_path, config_string)?;
         Ok(())
+    }
+
+    fn merge_config(config_string: &str, app_data_dir: &Path) -> Config {
+        let Ok(mut json_value) = serde_json::from_str::<serde_json::Value>(config_string) else {
+            return Config::default(app_data_dir);
+        };
+        let serde_json::Value::Object(ref mut map) = json_value else {
+            return Config::default(app_data_dir);
+        };
+        let Ok(default_config_value) = serde_json::to_value(Config::default(app_data_dir)) else {
+            return Config::default(app_data_dir);
+        };
+        let serde_json::Value::Object(default_map) = default_config_value else {
+            return Config::default(app_data_dir);
+        };
+        for (key, value) in default_map {
+            map.entry(key).or_insert(value);
+        }
+        let Ok(config) = serde_json::from_value(json_value) else {
+            return Config::default(app_data_dir);
+        };
+        config
+    }
+
+    fn default(app_data_dir: &Path) -> Config {
+        Config {
+            token: String::new(),
+            download_dir: app_data_dir.join("漫画下载"),
+            export_dir: app_data_dir.join("漫画导出"),
+            chapter_download_interval: 0,
+            download_with_author: false,
+            enable_file_logger: true,
+        }
     }
 }
