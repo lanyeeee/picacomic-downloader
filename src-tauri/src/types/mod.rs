f1fd88150dfed4370e8b0ac039680e46131eb6e3
lanyeeee <lanyeeeeee@gmail.com>
2025-05-01 05:36:09 +0800

    feat: 后端支持选择下载格式

--- src-tauri/src/types/mod.rs
+++ src-tauri/src/types/mod.rs
@@ -1,11 +1,13 @@
 mod chapter_info;
 mod comic;
 mod comic_info;
+mod downloaded_format;
 mod log_level;
 mod sort;
 
 pub use chapter_info::*;
 pub use comic::*;
 pub use comic_info::*;
+pub use downloaded_format::*;
 pub use log_level::*;
 pub use sort::*;
